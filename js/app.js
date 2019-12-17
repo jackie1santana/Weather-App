fb5cc3d5c3e0a2174488d1bd8512f34e394783ac
Jackie Santana <jackiebrown.nyc@gmail.comgit config --global user.email jackiebrown.nyc@gmail.com>
2019-12-16 20:23:08 -0600

    weather

--- js/app.js
+++ js/app.js
@@ -1,16 +1,34 @@
+
+
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/5abf12e75f7516f76fe1069a6eae5cec/29.8183,-95.5250'
 
-request({url: url, json:true} , (error, response) => {
-    console.log(response.body.currently)
-})
 
-const HoustonWeather = () => {
-    document.getElementById("houston").addEventListener("click", () => { 
-        console.log('work')
-    })
-}
+var url = 'https://api.darksky.net/forecast/5abf12e75f7516f76fe1069a6eae5cec/29.8183,-95.5250?units=us'
 
-HoustonWeather()
 
+
+
+   var req = () => {
+    request({url: url, json:true} , (error, response) => {
+    
+        var temp = response.body.currently
+        var newTemp = Object.keys(temp).map((key, index) => {
+            return console.log(temp[key])
+        })
+        
+     document.getElementById("houston").innerHTML = "hey"
+ })
+   }
+  
+
+
+
+
+// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamFja2llc2FudGFuYSIsImEiOiJjazQ4dXczd2IxOXd6M2VuNjliamhrZXg2In0.f1mca-KowMtSV757I8TrDQ'
+
+// request({url: geocodeURL, json:true} , (error, response) => {
+//     const latitude = response.body.features[0].center[1]
+//     const longitude = response.body.features[0].center[0]
+//     console.log(`${latitude} ${longitude}`)
+// }) 
